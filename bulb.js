31adbf251ec76b8351a3c2262076fc6725b68e87
hamzas07 <161144800+hamzas07@users.noreply.github.com>
2024-06-16 23:46:28 +0530

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,13 @@
+function change1(){
+	var x=document.getElementById('b1');
+	x.src="onbulb.png";
+var audio=document.getElementById("audio");
+audio.play();
+}
+function change2(){
+  var y=document.getElementById('b1');
+  y.src="offbulb.png"
+var audio=document.getElementById("audio");
+audio.play();
+}
+
